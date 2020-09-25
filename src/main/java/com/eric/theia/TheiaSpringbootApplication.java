2e65279365d2f4fe839c31fe850012953411b8d4
qixiang.chen <chenqx_gz@126.com>
2020-09-25 02:47:02 +0000

    Fully automate dev setup with Gitpod
    This commit implements a fully-automated development setup using Gitpod.io, an
    online IDE for GitHub and GitLab that enables Dev-Environments-As-Code.
    This makes it easy for anyone to get a ready-to-code workspace for any branch,
    issue or pull request almost instantly with a single click.

---
+++ src/main/java/com/eric/theia/TheiaSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.eric.theia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheiaSpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TheiaSpringbootApplication.class, args);
+    }
+    
+}
